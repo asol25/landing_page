af7a1a4a9a128ce4ae644e160d01790348ef6e6a
asol25 <usool.203@gmail.com>
2022-09-07 18:36:30 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.querySelector('.nav'),
+      navToggle = document.querySelector('.btn_toggle'),
+      navClose = document.querySelector('.close_toggle')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
